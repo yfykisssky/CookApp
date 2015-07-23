751c52db2f7aecebe707ecf703cc678c01e1acdc
jsw <jsw@jsw-PC>
2015-07-23 14:49:59 +0800

    修改

--- CookApp/src/com/cookapp/cookapp/contants/WebContants.java
+++ CookApp/src/com/cookapp/cookapp/contants/WebContants.java
@@ -5,4 +5,12 @@ public class WebContants {
 	final public static String SERVICE_URL="";
 	
 	final public static String UPDATE_VERSION_URL=SERVICE_URL+"";
+	
+	final public static String LOGIN_CHECK_URL=SERVICE_URL+"";
+	
+	final public static String REGISTER_URL=SERVICE_URL+"";
+	
+	final public static String FINDBACK_PASWD_URL=SERVICE_URL+"";
+	
+	final public static String FEEDBACK_URL=SERVICE_URL+"";
 }
